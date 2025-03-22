648dc5df77666444876820646ecc19c92c917caa
MafeSaavedra <mariafernanda.saavedra@genstudents.org>
2025-03-22 12:35:38 -0500

    Page principal done!

--- ToDoList/ToDoList/src/PagPrincipal/PagPrincipal.jsx
+++ ToDoList/ToDoList/src/PagPrincipal/PagPrincipal.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import './PagPrincipal.css';
 import Gif from '../Img/Pinguino.gif';
+import Check from '../Img/Check-logo.png';
+
 
 export function PagPrincipal() {
     return (
-        <section className='ContenedorPrincipal'>
-            <div>
-                <h1>LISTO </h1>
-            </div>
-            <div>
-
-                <img src={Gif} />
-
-            </div>
+        <section className='ContenedorFondo'>
+            <section className='ContenedorPrincipal'>
+                <div className='Principal-Letras'>
+                    <div>
+                        <h1>
+                            LIST<img src={Check} className="CheckLogo" />
+                        </h1>
+                    </div>
+                    <div>
+                        <h1>Para llevar </h1>
+                    </div>
+                    <div>
+                        <h1>TU PRODUCTIVIDAD </h1>
+                    </div>
+                    <div>
+                        <h1>A otro nivel </h1>
+                    </div>
+                </div>
+                <div className='PrincipalGif'>
+                    <img src={Gif} />
+                </div>
+            </section>
         </section>
+
     );
 }
