8dac2f381ee89e8a2c869b43b28b464168fbd47a
MafeSaavedra <mariafernanda.saavedra@genstudents.org>
2025-03-24 20:49:27 -0500

    Update To Do List

--- ToDoList/ToDoList/src/Componentes/Tarea/Tarea.jsx
+++ ToDoList/ToDoList/src/Componentes/Tarea/Tarea.jsx
@@ -2,35 +2,43 @@ import React, { useState } from "react";
 import './Tarea.css';
 
 export function Tarea() {
-    const [hora, setHora] = useState => (" 00 : 00");
-    const [periodo, setPeriodo] = useState => ("AM");
-    const [input, setInput] = useState => ("Nueva Tarea");
+    const [hora, setHora] = useState("00:00");
+    const [periodo, setPeriodo] = useState("AM");
+    const [input, setInput] = useState("Nueva Tarea");
+    const [check, setCheck] = useState(false);
+
     return (
         <section className="sectionTarea">
             <div className="TareaCheck">
-                <input type="checkbox" />
-
+                <input type="checkbox"
+                    checked={check}
+                    onChange={(e) => {
+                        console.log("Checkbox cambiado:", e.target.checked);
+                        setCheck(e.target.checked);
+                    }} />
             </div>
 
             <div className="TareaTime">
                 <input
+                    className="TareaTime-Hora"
                     type="text"
                     value={hora}
-                    /* Setea el estado cuando ocurre un cambio Onchange */
+                    /* Setea el estado, cuando ocurre un cambio Onchange */
                     onChange={(e) => setHora(e.target.value)}
                     pattern="(0[1-9]|1[0-2]):[0-5][0-9]"
                 />
 
-                <select value={periodo} onChange={(e) => setPeriodo(e.target.value)}>
+                <select className="TareaTime-Periodo" value={periodo} onChange={(e) => setPeriodo(e.target.value)}>
                     <option>AM</option>
                     <option>PM</option>
                 </select>
             </div>
 
-            <div className="TareaInput">
+            <div className="TareaInputDiv">
                 <input
                     type="text"
                     value={input}
+                    onChange={(e) => setInput(e.target.value)}
                     className="TareaInput"
                 />
             </div>
--- ToDoList/ToDoList/src/ToDo/ToDo.jsx
+++ ToDoList/ToDoList/src/ToDo/ToDo.jsx
@@ -4,6 +4,8 @@ import './ToDo.css';
 import { Fecha } from '../Componentes/Fecha/Fecha.jsx';
 import { Notas } from '../Componentes/NotasImport/Notas.jsx';
 import { Boton } from '../Componentes/Boton/BotonLila.jsx';
+import { Tarea } from '../Componentes/Tarea/Tarea.jsx';
+
 import { BotonConfig } from '../Componentes/BotonConfig/BotonConfig.jsx';
 
 export function ToDo() {
@@ -27,13 +29,11 @@ export function ToDo() {
                     <Boton children="Crear Nueva Tarea" />
                     <Boton children="Seleccionar Todo" />
                     <BotonConfig />
+                </div>
+                <div className='ToDoCuadro-Tarea'>
+                    <Tarea />
+                </div>
 
-                    <h1>Tareas</h1>
-                </div>
-                <div className='CRUD'>
-                    {/* Aqui va el componente de todo lista con la opcion de check (compleete)
-                   la tarea y el boton de eliminar  */}
-                </div>
             </section>
             <section className='ContToDo-notas'>
                 <div className='ConToDo-Fecha'>
